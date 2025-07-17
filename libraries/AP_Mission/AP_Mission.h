ed8310d7fc99e9beb457daf44a7b3ce4c60a5f48
jd-hanley <joseph.hanley@colorado.edu>
2025-07-17 11:17:03 -0600

    Removed more sprayer stuff

--- libraries/AP_Mission/AP_Mission.h
+++ libraries/AP_Mission/AP_Mission.h
@@ -945,8 +945,6 @@ private:
     bool start_command_camera(const AP_Mission::Mission_Command& cmd);
     bool start_command_parachute(const AP_Mission::Mission_Command& cmd);
     bool command_do_set_repeat_dist(const AP_Mission::Mission_Command& cmd);
-
-    bool start_command_do_sprayer(const AP_Mission::Mission_Command& cmd);
     bool start_command_do_scripting(const AP_Mission::Mission_Command& cmd);
     bool start_command_do_gimbal_manager_pitchyaw(const AP_Mission::Mission_Command& cmd);
     bool start_command_fence(const AP_Mission::Mission_Command& cmd);
