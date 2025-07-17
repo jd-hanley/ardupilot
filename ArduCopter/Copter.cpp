006738b5ebfff6299945fbc7628790c37d78e405
jd-hanley <joseph.hanley@colorado.edu>
2025-07-17 11:09:12 -0600

    Removed another reference to sprayer

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -184,9 +184,7 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
 #if MODE_SMARTRTL_ENABLED
     SCHED_TASK_CLASS(ModeSmartRTL,         &copter.mode_smartrtl,       save_position,    3, 100,  51),
 #endif
-#if HAL_SPRAYER_ENABLED
-    SCHED_TASK_CLASS(AC_Sprayer,           &copter.sprayer,               update,         3,  90,  54),
-#endif
+
     SCHED_TASK(three_hz_loop,          3,     75, 57),
 #if AP_SERVORELAYEVENTS_ENABLED
     SCHED_TASK_CLASS(AP_ServoRelayEvents,  &copter.ServoRelayEvents,      update_events, 50,  75,  60),
