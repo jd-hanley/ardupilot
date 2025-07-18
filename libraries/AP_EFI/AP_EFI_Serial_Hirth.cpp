b7eec47fc1375e922a38b8f377900cead7f364ea
jd-hanley <joseph.hanley@colorado.edu>
2025-07-18 12:16:04 -0600

    Removed references to ICEngine

--- libraries/AP_EFI/AP_EFI_Serial_Hirth.cpp
+++ libraries/AP_EFI/AP_EFI_Serial_Hirth.cpp
@@ -22,7 +22,6 @@
 #include <AP_EFI/AP_EFI_Serial_Hirth.h>
 #include <AP_SerialManager/AP_SerialManager.h>
 #include <SRV_Channel/SRV_Channel.h>
-#include <AP_ICEngine/AP_ICEngine.h>
 #include <AP_Math/definitions.h>
 #include <AP_Logger/AP_Logger.h>
 
@@ -167,12 +166,7 @@ void AP_EFI_Serial_Hirth::send_request()
         // send new throttle value, only when ARMED
         bool allow_throttle = hal.util->get_soft_armed();
         if (!allow_throttle) {
-#if AP_ICENGINE_ENABLED
-            const auto *ice = AP::ice();
-            if (ice != nullptr) {
-                allow_throttle = ice->allow_throttle_while_disarmed();
-            }
-#endif  // AP_ICENGINE_ENABLED
+            
         }
         if (allow_throttle) {
             request_was_sent = send_target_values(new_throttle);
