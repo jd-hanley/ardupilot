15636b2681248cc885ee3af813f1c99a00575602
jd-hanley <joseph.hanley@colorado.edu>
2025-08-14 09:27:27 -0600

    added delay back in

--- libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
+++ libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
@@ -49,6 +49,7 @@ void setup()
 void loop()
 {  
     oflow.printFlow();
+    hal.scheduler->delay(100);
     // int32_t* data_arm_0 = strain.get_data(0);
     // uint32_t last_update_arm_0 = strain.get_last_update(0);
 
