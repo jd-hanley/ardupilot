e7cb1858a8e278676762c0d2155762576aede6f3
jd-hanley <joseph.hanley@colorado.edu>
2025-08-19 12:17:31 -0600

    Update RFIND_test.cpp

--- libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
+++ libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
@@ -49,7 +49,7 @@ void setup()
 void loop()
 {  
     oflow.printTest();
-    hal.scheduler->delay(200);
+    hal.scheduler->delay(50);
     // int32_t* data_arm_0 = strain.get_data(0);
     // uint32_t last_update_arm_0 = strain.get_last_update(0);
 
