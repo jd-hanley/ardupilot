fe2afce4264418b4c1bdb9162ec3c455081a0996
jd-hanley <joseph.hanley@colorado.edu>
2025-08-19 11:25:37 -0600

    Timing test

--- libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
+++ libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
@@ -48,8 +48,8 @@ void setup()
 
 void loop()
 {  
-    oflow.printFlow();
-    hal.scheduler->delay(100);
+    // oflow.printFlow();
+    // hal.scheduler->delay(100);
     // int32_t* data_arm_0 = strain.get_data(0);
     // uint32_t last_update_arm_0 = strain.get_last_update(0);
 
