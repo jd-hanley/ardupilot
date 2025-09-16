64660d04b883185fc1689ec4b65e94b98d2cd1fc
jd-hanley <joseph.hanley@colorado.edu>
2025-09-16 14:19:37 -0600

    Update RFIND_test.cpp

--- libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
+++ libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
@@ -48,8 +48,9 @@ void setup()
 
 void loop()
 {  
-    GCS_SEND_TEXT(MAV_SEVERITY_INFO, "RFIND setup starting");
+    
     oflow.printFlow();
+    hal.console->printf("--------------------------------------");
     hal.scheduler->delay(500);
     // int32_t* data_arm_0 = strain.get_data(0);
     // uint32_t last_update_arm_0 = strain.get_last_update(0);
