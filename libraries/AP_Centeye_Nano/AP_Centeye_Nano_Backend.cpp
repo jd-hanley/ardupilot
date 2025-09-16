5fee01514baad9cf44cb21222f2807a2a3986a0c
jd-hanley <joseph.hanley@colorado.edu>
2025-09-16 14:48:21 -0600

    Update AP_Centeye_Nano_Backend.cpp

--- libraries/AP_Centeye_Nano/AP_Centeye_Nano_Backend.cpp
+++ libraries/AP_Centeye_Nano/AP_Centeye_Nano_Backend.cpp
@@ -42,10 +42,10 @@ void AP_Centeye_Nano_Backend::timer()
     // Note that this method will need to be revisited in the future to add robust status checks
     // For now, here is the bare bones implementation
 
-    // uint32_t t0 = AP_HAL::micros();
+    uint32_t t0 = AP_HAL::micros();
     get_data();
-    // uint32_t dur = AP_HAL::micros() - t0;
-    // hal.console->printf("Duration: %ld\n", dur);
+    uint32_t dur = AP_HAL::micros() - t0;
+    hal.console->printf("Duration: %ld\n", dur);
 }
 
 bool AP_Centeye_Nano_Backend::get_data()
