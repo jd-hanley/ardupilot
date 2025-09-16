f43bae3200760296ea892094693bda37ee4cd106
jd-hanley <joseph.hanley@colorado.edu>
2025-09-16 14:29:16 -0600

    Update RFIND_test.cpp

--- libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
+++ libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
@@ -26,7 +26,7 @@ static RangeFinder sonar;
 
 static AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev_temp = nullptr;
 // static AP_Strain strain;
-// static AP_Centeye_Nano oflow;
+static AP_Centeye_Nano oflow;
 
 int32_t count = 0;
 void setup()
@@ -40,8 +40,8 @@ void setup()
 
     // oflow
     // hal.scheduler->delay(5000)
-    // hal.console->printf("Optical flow test\n");
-    // oflow.init();
+    hal.console->printf("Optical flow test\n");
+    oflow.init();
 
 
 }
@@ -49,7 +49,7 @@ void setup()
 void loop()
 {  
     
-    // oflow.printFlow();
+    oflow.printFlow();
     hal.console->printf("--------------------------------------");
     hal.scheduler->delay(500);
     // int32_t* data_arm_0 = strain.get_data(0);
