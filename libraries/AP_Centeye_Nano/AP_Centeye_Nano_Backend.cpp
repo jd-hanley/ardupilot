784df38f93fd2e9f5ee381283f5fc8844b60d926
jd-hanley <joseph.hanley@colorado.edu>
2025-09-16 15:42:20 -0600

    Debug

--- libraries/AP_Centeye_Nano/AP_Centeye_Nano_Backend.cpp
+++ libraries/AP_Centeye_Nano/AP_Centeye_Nano_Backend.cpp
@@ -29,7 +29,7 @@ bool AP_Centeye_Nano_Backend::write_bytes(uint8_t* bytes, uint8_t length)
 bool AP_Centeye_Nano_Backend::init()
 {
     // Call timer at 60 hz
-    _dev->register_periodic_callback(400000, FUNCTOR_BIND_MEMBER(&AP_Centeye_Nano_Backend::timer, void));
+    _dev->register_periodic_callback(16700, FUNCTOR_BIND_MEMBER(&AP_Centeye_Nano_Backend::timer, void));
 
     // If there is any additional calibration we want to do, it should go here
     return true;
@@ -58,18 +58,18 @@ bool AP_Centeye_Nano_Backend::get_data()
     // uint32_t dur = AP_HAL::millis() - t0;
     // hal.console->printf("Duration: %ld\n", dur);
     // t0 = AP_HAL::millis(); 
-    if (!read_objdet_h())
-    {
-        // Error handling goes here for failure to read objdet
+    // if (!read_objdet_h())
+    // {
+    //     // Error handling goes here for failure to read objdet
         
-    }
-    // dur = AP_HAL::millis() - t0;
-    // hal.console->printf("Duration: %ld\n", dur);
-    // t0 = AP_HAL::millis(); 
-    if (!read_objdet_v())
-    {
-        // Error handling goes here for failure to read objdet
-    }
+    // }
+    // // dur = AP_HAL::millis() - t0;
+    // // hal.console->printf("Duration: %ld\n", dur);
+    // // t0 = AP_HAL::millis(); 
+    // if (!read_objdet_v())
+    // {
+    //     // Error handling goes here for failure to read objdet
+    // }
     // dur = AP_HAL::millis() - t0;
     // hal.console->printf("Duration: %ld\n", dur);
     // if (!read_oflow())
@@ -101,7 +101,7 @@ bool AP_Centeye_Nano_Backend::read_odom()
         if (!write_bytes(command, 2))
         {
             // Error handling goes here...
-            // hal.console->printf("Write failed\n");
+            hal.console->printf("Write failed\n");
         }
         uint8_t buffer[ODOM_BYTES];
         int32_t old_odom_x = unsafe_data.odom_x;
