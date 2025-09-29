c148c638bbc28ab83f0ede864e7c25b2890177cf
jd-hanley <joseph.hanley@colorado.edu>
2025-09-29 12:28:33 -0600

    Added test for system time broadcast

--- libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
+++ libraries/AP_RangeFinder/examples/RFIND_test/RFIND_test.cpp
@@ -24,11 +24,11 @@ const AP_HAL::HAL& hal = AP_HAL::get_HAL();
 static AP_SerialManager serial_manager;
 static RangeFinder sonar;
 
-static AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev_temp = nullptr;
+// static AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev_temp = nullptr;
 // static AP_Strain strain;
-static AP_Centeye_Nano oflow;
+// static AP_Centeye_Nano oflow;
 
-int32_t count = 0;
+// int32_t count = 0;
 void setup()
 {
 
@@ -40,18 +40,46 @@ void setup()
 
     // oflow
     // hal.scheduler->delay(5000);
-    hal.console->printf("Optical flow test\n");
-    oflow.init();
+    // hal.console->printf("Optical flow test\n");
+    // oflow.init();
 
+    // Set up I2C communication for Testing Interrupt
+    AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev_temp = hal.i2c_mgr->get_device(0, 0x12);
 
 }
 
 void loop()
 {  
-    
-    oflow.printTest();
-    // hal.console->printf("--------------------------------------");
+    bool success;
+    // System time broadcast testing:
+    // Need to do the following:
+    //      Store the current system time in ms via mills()
+    //      Break the result into bytes
+    //      Transfer the bytes via I2C
+    //      Delay to impose some sort of frequency
+    uint8_t num_bytes = 4;
+    uint32_t curr_time = AP_HAL::millis();
+    uint8_t bytes[num_bytes];
+
+    for (uint8_t i = 0; i < 4; i++)
+    {
+        bytes[i] = (curr_time >> (8 * (num_bytes - 1 - i))) & 0xFF;
+    }
+    success = dev_temp->transfer(bytes, num_bytes, NULL, 0);
+    if (success)
+    {
+        hal.console->printf("Successfully broadcast system time\n");
+    }
+    else
+    {
+        hal.console->printf("Failed to broadcast system time\n");
+    }
     hal.scheduler->delay(200);
+
+
+    // oflow.printTest();
+    // // hal.console->printf("--------------------------------------");
+    // hal.scheduler->delay(200);
     // int32_t* data_arm_0 = strain.get_data(0);
     // uint32_t last_update_arm_0 = strain.get_last_update(0);
 
