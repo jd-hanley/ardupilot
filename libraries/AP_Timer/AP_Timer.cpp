f6c17ffd88f15828dc26e7169707998bae9a4dcb
jd-hanley <joseph.hanley@colorado.edu>
2025-10-01 12:32:15 -0600

    Forgot to add semaphore

--- libraries/AP_Timer/AP_Timer.cpp
+++ libraries/AP_Timer/AP_Timer.cpp
@@ -40,21 +40,30 @@ bool AP_Timer::broadcast_millis()
     uint32_t sysTime = AP_HAL::millis();
     // Break the system time into bytes to be transferred in big endian order
     uint8_t bytes[NUM_BYTES];
-    for (uint8_t i = 0; i < NUM_BYTES; i++)
+    bool has_sem = _dev->get_semaphore()->take(20);
+    if (has_sem)
     {
-        // Shift right an appropriate amount and mask
-        bytes[i] = (sysTime >> ((NUM_BYTES - i - 1) * 8)) & 0xFF;
-    }
-    if (!write_bytes(bytes, NUM_BYTES))
-    {
-        // Writing bytes failed for some reason, needs to be handled here.
-        hal.console->printf("Failed to broadcast system time\n");
-        return false;
+        for (uint8_t i = 0; i < NUM_BYTES; i++)
+        {
+            // Shift right an appropriate amount and mask
+            bytes[i] = (sysTime >> ((NUM_BYTES - i - 1) * 8)) & 0xFF;
+        }
+        if (!write_bytes(bytes, NUM_BYTES))
+        {
+            // Writing bytes failed for some reason, needs to be handled here.
+            hal.console->printf("Failed to broadcast system time\n");
+            return false;
+        }
+        else
+        {
+            hal.console->printf("Successfully broadcast system time\n");
+            return true;
+        }
     }
     else
     {
-        hal.console->printf("Successfully broadcast system time\n");
-        return true;
+        // Failed to obtain semaphore
+        hal.console->printf("Failed to obtain semaphore\n");
     }
 }
 
@@ -64,21 +73,30 @@ bool AP_Timer::broadcast_micros()
     uint32_t sysTime = AP_HAL::micros();
     // Break the system time into bytes to be transferred in big endian order
     uint8_t bytes[NUM_BYTES];
-    for (uint8_t i = 0; i < NUM_BYTES; i++)
+    bool has_sem = _dev->get_semaphore()->take(20);
+    if (has_sem)
     {
-        // Shift right an appropriate amount and mask
-        bytes[i] = (sysTime >> ((NUM_BYTES - i - 1) * 8)) & 0xFF;
-    }
-    if (!write_bytes(bytes, NUM_BYTES))
-    {
-        // Writing bytes failed for some reason, needs to be handled here.
-        hal.console->printf("Failed to broadcast system time\n");
-        return false;
-    }
+        for (uint8_t i = 0; i < NUM_BYTES; i++)
+        {
+            // Shift right an appropriate amount and mask
+            bytes[i] = (sysTime >> ((NUM_BYTES - i - 1) * 8)) & 0xFF;
+        }
+        if (!write_bytes(bytes, NUM_BYTES))
+        {
+            // Writing bytes failed for some reason, needs to be handled here.
+            hal.console->printf("Failed to broadcast system time\n");
+            return false;
+        }
+        else
+        {
+            hal.console->printf("Successfully broadcast system time\n");
+            return true;
+        }
+    }   
     else
     {
-        hal.console->printf("Successfully broadcast system time\n");
-        return true;
+        // Failed to get semaphore
+        hal.console->printf("Failed to obtain semaphore\n");
     }
 }
 
