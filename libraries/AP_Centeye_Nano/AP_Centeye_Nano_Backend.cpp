b1a755cd1bea4433154518b27cf43d3aa0d43079
jd-hanley <joseph.hanley@colorado.edu>
2025-10-03 13:38:25 -0600

    Trying to figure out why objdet_h is breaking again

--- libraries/AP_Centeye_Nano/AP_Centeye_Nano_Backend.cpp
+++ libraries/AP_Centeye_Nano/AP_Centeye_Nano_Backend.cpp
@@ -67,16 +67,19 @@ bool AP_Centeye_Nano_Backend::get_data()
     }
     else
     {
-        // hal.console->printf("Successfully read odometry data\n");
+        hal.console->printf("Successfully read odometry data\n");
     }
     // uint32_t dur = AP_HAL::micros() - t0;
     // hal.console->printf("Duration: %ld\n", dur);
     // t0 = AP_HAL::micros(); 
-    // if (!read_objdet_h())
-    // {
-    //     // Error handling goes here for failure to read objdet
-        
-    // }
+    if (!read_objdet_h())
+    {
+        // Error handling goes here for failure to read objdet
+    }
+    else
+    {
+        hal.console->printf("Successfully read objdet_h data\n");
+    }
     // // dur = AP_HAL::micros() - t0;
     // // hal.console->printf("Duration: %ld\n", dur);
     // // t0 = AP_HAL::micros(); 
@@ -130,7 +133,7 @@ bool AP_Centeye_Nano_Backend::read_odom()
         unsafe_data.odom_y = (uint32_t) buffer[7] << 24 | (uint32_t) buffer[6] << 16 | (uint32_t) buffer[5] << 8 | (uint32_t) buffer[4];
         unsafe_data.odom_div = (uint32_t) buffer[11] << 24 | (uint32_t) buffer[10] << 16 | (uint32_t) buffer[9] << 8 | (uint32_t) buffer[8];
         // Calculate the time step in seconds
-        hal.console->printf("%ld\n",unsafe_data.odom_x);
+        // hal.console->printf("%ld\n",unsafe_data.odom_x);
         // float dt = ((float) current_time - (float) unsafe_data.meas_time) / 1000.0;
         unsafe_data.meas_time = current_time;
         unsafe_data.flow_x = (((float) unsafe_data.odom_x - (float) old_odom_x)) / 1000;
