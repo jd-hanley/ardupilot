3b7e90a12c7bf8960f9de15e494a63b55da5d5a5
jd-hanley <joseph.hanley@colorado.edu>
2025-11-17 14:08:52 -0700

    Update system.cpp

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -460,7 +460,7 @@ void Copter::allocate_motors(void)
     }
     AP_Param::load_object_from_eeprom(attitude_control, attitude_control_var_info);
         
-    pos_control = NEW_NOTHROW AC_PosControl(*ahrs_view, inertial_nav, *motors, *attitude_control, strain);
+    pos_control = NEW_NOTHROW AC_PosControl(*ahrs_view, inertial_nav, *motors, *attitude_control);
     if (pos_control == nullptr) {
         AP_BoardConfig::allocation_error("PosControl");
     }
