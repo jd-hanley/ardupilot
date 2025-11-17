0665015924c02c9ed4b842fbfc56b68fd5ade746
jd-hanley <joseph.hanley@colorado.edu>
2025-11-17 14:01:55 -0700

    Update AP_Vehicle.h

--- libraries/AP_Vehicle/AP_Vehicle.h
+++ libraries/AP_Vehicle/AP_Vehicle.h
@@ -341,8 +341,8 @@ protected:
 #endif
     AP_Baro barometer;
     // created by Ian 
-    AP_Strain strain;
-    AP_Centeye_Nano centeye_nano;
+    // AP_Strain strain;
+    // AP_Centeye_Nano centeye_nano;
     AP_Timer timer_1;
     AP_Timer timer_2;
     
